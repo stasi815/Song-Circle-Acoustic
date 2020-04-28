961d39761185e5da1eb7ded3b2c6175d264072dc
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-04-28 11:13:56 -0700

    header component built

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Title.css'
+
+function Title() {
+    return (
+        <div className="Title">
+            <header>
+                <h1>Song Circle Acoustic</h1>
+                <div className="Title-Subtitle">Sacred Songs for Acoustic Guitar</div>
+            </header>
+        </div>
+    )
+}
+
+export default Title
