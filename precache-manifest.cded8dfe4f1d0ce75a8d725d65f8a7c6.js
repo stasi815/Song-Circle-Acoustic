00834c227629c3fe6a98ef64c31710a87a2dd1ce
stasi815 <anastasia.gallardo@students.makeschool.com>
2020-05-14 09:14:57 -0700

    Updates

---
+++ precache-manifest.cded8dfe4f1d0ce75a8d725d65f8a7c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840ec2b30ffc5a44a13c597b85584033",
+    "url": "/Song-Circle-Acoustic/index.html"
+  },
+  {
+    "revision": "416792991d8c65dae8b4",
+    "url": "/Song-Circle-Acoustic/static/css/main.d2b4ebaa.chunk.css"
+  },
+  {
+    "revision": "f35437fbe8bc51ff7de5",
+    "url": "/Song-Circle-Acoustic/static/js/2.b16ac58e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Song-Circle-Acoustic/static/js/2.b16ac58e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "416792991d8c65dae8b4",
+    "url": "/Song-Circle-Acoustic/static/js/main.494bf5c6.chunk.js"
+  },
+  {
+    "revision": "846d28a24ce078fff1f8",
+    "url": "/Song-Circle-Acoustic/static/js/runtime-main.0be00051.js"
+  }
+]);
